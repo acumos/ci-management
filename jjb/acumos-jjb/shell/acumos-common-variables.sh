ca969f9512b34ac2d94cced8c7db8a7182b2a09d
Christopher Lott (cl778h) <clott@research.att.com>
2017-12-21 16:25:54 -0500

    Add templates with docker-login builder
    Add acumos-jjb directory.
    Replicate and extend the merge, release, verify templates
      from global-jjb/jjb/lf-maven-jobs.yaml with docker-login builder.
    Replicate common-variables shell script to use log level "info"
      and to pass Jenkins build number.
    Remove useless acumos-mvn-infra-opts from defaults.yaml.
    
    Change-Id: I3df560def37576e6c1a19f71af8182e31379e273
    Signed-off-by: Christopher Lott (cl778h) <clott@research.att.com>

---
+++ jjb/acumos-jjb/shell/acumos-common-variables.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# This file contains a list of variables that are generally useful in many
+# scripts. It is meant to be sourced in other scripts so that the variables can
+# be called.
+
+# This Acumos version modifies original in global-jjb/shell:
+#  - uses log level INFO to show maven download progress messages
+#  - publishes maven property with Jenkins build number
+
+MAVEN_OPTIONS="$(echo --show-version \
+    --batch-mode \
+    -Djenkins \
+    -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=info \
+    -Dbuild.number=${BUILD_NUMBER} \
+    -Dmaven.repo.local=/tmp/r \
+    -Dorg.ops4j.pax.url.mvn.localRepository=/tmp/r)"
+echo "$MAVEN_OPTIONS"
