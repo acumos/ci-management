d24bfbfdc35632e92c10ed01f6e9d3912086789b
Christopher Lott (cl778h) <clott@research.att.com>
2018-01-08 19:31:51 -0500

    Add maven-package script.
    Revise docker-enabled verify job to use goals clean package,
    not clean deploy.  The global-jjb maven-build script uses goal
    deploy, which causes the image to be deployed on verify, an
    undesirable side effect.  Add a shell script with goals clean
    package so that the image is only built, not pushed.
    
    Change-Id: I13007efc5e9c74c85512f5251ad3e1b9c2062dc7
    Signed-off-by: Christopher Lott (cl778h) <clott@research.att.com>

---
+++ jjb/acumos-jjb/shell/maven-package.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# This script builds a Maven project and packages artifacts.
+
+# DO NOT enable -u because $MAVEN_PARAMS and $MAVEN_OPTIONS could be unbound.
+# Ensure we fail the job if any steps fail.
+set -e -o pipefail
+set +u
+
+export MAVEN_OPTS
+
+# Disable SC2086 because we want to allow word splitting for $MAVEN_* parameters.
+# shellcheck disable=SC2086
+$MVN clean package \
+    --global-settings "$GLOBAL_SETTINGS_FILE" \
+    --settings "$SETTINGS_FILE" \
+    $MAVEN_OPTIONS $MAVEN_PARAMS
