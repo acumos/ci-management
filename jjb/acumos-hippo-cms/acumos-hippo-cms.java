5d8950e15cc4134711374ca197632f4a88294f48
Christopher Lott (cl778h) <clott@research.att.com>
2018-01-11 10:50:52 -0500

    Revise jobs for docker
    Use the Acumos-custom docker-enabled job templates.
    Drop whitespace from common-dataservice.
    
    Change-Id: Idb567b004baed24f6dc9676d4b487bd70478d543
    Signed-off-by: Christopher Lott (cl778h) <clott@research.att.com>

--- jjb/acumos-hippo-cms/acumos-hippo-cms.java
+++ jjb/acumos-hippo-cms/acumos-hippo-cms.java
@@ -1,14 +1,22 @@
 ---
 - project:
     name: acumos-hippo-cms-project
+    # jenkins job name prefix
     project-name: acumos-hippo-cms
+    # git repo
     project: acumos-hippo-cms
     mvn-settings: acumos-hippo-cms-settings
+    # pass Jenkins build number
+    mvn-params: -Dbuild.number=${{BUILD_NUMBER}}
+    # needs docker-enabled build node
+    build-node: centos7-docker-4c-4g
     jobs:
-      - '{project-name}-maven-jobs'
-      # merge job is not yet part of global JJB
-      - gerrit-maven-merge
-      # TODO: ADD DOCKER
+      - gerrit-maven-dl-verify
+      - gerrit-maven-dl-merge
+      - gerrit-maven-dl-release
+      # javadoc group has -publish and -verify
+      - '{project-name}-maven-javadoc-jobs'
+      - gerrit-maven-sonar
     stream:
       - master:
           branch: master
