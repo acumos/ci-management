e5758a94ee979ec6d5004c01fa3ad5f6746b27cc
Christopher Lott (cl778h) <clott@research.att.com>
2018-01-18 13:34:14 -0500

    Use maven goal test for sonar
    Replicates Global JJB anchor, builder and template for Sonar but
    calls a custom shell script which invokes maven just once with goals
    "clean package sonar:sonar" instead of twice with goal deploy.
    This avoids building docker images.
    
    Change-Id: Ia571b480cbc9a28c1c9ab3855fc37aa52a5a0402
    Signed-off-by: Christopher Lott (cl778h) <clott@research.att.com>

---
+++ jjb/acumos-jjb/shell/acumos-maven-sonar.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+
+# This script builds a Maven project and deploys it into a staging repo which
+# can be used to deploy elsewhere later eg. Nexus staging / snapshot repos.
+
+# DO NOT enable -u because $MAVEN_PARAMS and $MAVEN_OPTIONS could be unbound.
+# Ensure we fail the job if any steps fail.
+set -xe -o pipefail
+set +u
+
+export MAVEN_OPTS
+
+# https://stackoverflow.com/questions/31024392/what-maven-goal-is-required-to-run-maven-sonar-analysis
+# Disable SC2086 because we want to allow word splitting for $MAVEN_* parameters.
+# shellcheck disable=SC2086
+$MVN clean package $SONAR_MAVEN_GOAL \
+    -e -Dsonar -Dsonar.host.url="$SONAR_HOST_URL" \
+    --global-settings "$GLOBAL_SETTINGS_FILE" \
+    --settings "$SETTINGS_FILE" \
+    -DaltDeploymentRepository=staging::default::file:"$WORKSPACE"/m2repo \
+    $MAVEN_OPTIONS $MAVEN_PARAMS
