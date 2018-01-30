938ab199168dd63fcd9e5d64eaf65fec79097d70
Christopher Lott (cl778h) <clott@research.att.com>
2018-01-30 16:55:08 -0500

    Use pom for name and tag
    Revise docker build script to pull image name and tag from pom.xml;
    also restore use of shell script that strips -SNAPSHOT suffix in
    the stage job.
    
    Change-Id: I4e540e382a87fc5c393548c05f663fd9bbed8bec
    Signed-off-by: Christopher Lott (cl778h) <clott@research.att.com>

--- jjb/acumos-jjb/shell/docker-build.sh
+++ jjb/acumos-jjb/shell/docker-build.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 # Ensure we fail the job if any steps fail
 # Do not set -u as DOCKER_ARGS may be unbound
-set -e -o pipefail
+set -e -x -o pipefail
 
+# Switch to the directory with the Dockerfile and pom
+cd "$DOCKER_ROOT"
+
+# Use artifactId as image name
+if [[ -z "$DOCKER_NAME" ]]; then
+    DOCKER_NAME=$( xmlstarlet sel -N "x=http://maven.apache.org/POM/4.0.0" -t -v "/x:project/x:artifactId" pom.xml )
+fi
+
+# Use version as image tag
 if [[ -z "$DOCKER_TAG" ]]; then
     DOCKER_TAG=$( xmlstarlet sel -N "x=http://maven.apache.org/POM/4.0.0" -t -v "/x:project/x:version" pom.xml )
 fi
-
-# Switch to the directory where the Dockerfile is
-cd "$DOCKER_ROOT"
 
 # DOCKERREGISTRY is purposely not using an '_' so as to not conflict with the
 # Jenkins global env var of the DOCKER_REGISTRY which the docker-login step uses
@@ -23,4 +29,3 @@ docker build $DOCKER_ARGS . -t $IMAGE_NAME | tee "$WORKSPACE/docker_build_log.tx
 # Write DOCKER_IMAGE information to a file so it can be injected into the
 # environment for following steps
 echo "DOCKER_IMAGE=$IMAGE_NAME" >> "$WORKSPACE/env_inject.txt"
-
