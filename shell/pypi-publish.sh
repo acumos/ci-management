f19c58a0c9f3c62be4d8fa23a5c940b49388bc1a
Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>
2018-02-14 10:27:24 -0600

    Configure PyPI releases
    This changes adds the following functionality for PyPI releases
    
    - macros
     - A PyPI server macro
     - A timed release macro
    
    - scripts
     - A script to build and publish PyPI artifacts
    
    - templates
     - Templates for staging and release
    
    - jobs
     - Jobs for staging and release
    
    Change-Id: I4689ae70caf5b419bbdc18caab172ac63b69d9c0
    Signed-off-by: Vanessa Rene Valderrama <vvalderrama@linuxfoundation.org>

---
+++ shell/pypi-publish.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Script to push PyPI artifacts 
+
+# Create and activate virtualenv
+virtualenv /tmp/v/twine
+source "/tmp/v/twine/bin/activate"
+
+# Install twine
+pip install twine
+
+# Install wheel
+pip install wheel
+
+# Create distribution
+python setup.py sdist bdist_wheel
+
+# Upload artifacts
+twine upload -r $PYPI_SERVER dist/*
