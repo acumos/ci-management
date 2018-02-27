9013a8075a0a940c1373173fa07d22c8551b0d21
Christopher Lott (cl778h) <clott@research.att.com>
2018-02-27 13:42:43 -0500

    Rsync docs to AWS
    First draft of custom-to-project JJB template for documentation
    merge job that pushes generated docs using rsync to AWS host
    acumos-privdocs-rsync-elb-1197834036.us-west-2.elb.amazonaws.com
    
    Open question: merge doesn't seem to build the docs (?)
    
    Change-Id: I941744f7f88bbbea125070692482c15f7bbb2eb2
    Signed-off-by: Christopher Lott (cl778h) <clott@research.att.com>
    Signed-off-by: Jeremy Phelps <jphelps@linuxfoundation.org>

---
+++ jjb/acumos-jjb/shell/tox-install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# SPDX-License-Identifier: EPL-1.0
+##############################################################################
+# Copyright (c) 2017 The Linux Foundation and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+##############################################################################
+echo "---> tox-install.sh"
+
+# Ensure we fail the job if any steps fail.
+# DO NOT set -u as virtualenv's activate script has unbound variables
+set -e -o pipefail
+
+virtualenv --quiet -p "$PYTHON_VERSION" "/tmp/v/tox"
+# shellcheck source=/tmp/v/tox/bin/activate disable=SC1091
+source "/tmp/v/tox/bin/activate"
+pip install --quiet --upgrade pip
+pip install --quiet --upgrade pipdeptree
+pip install --quiet --upgrade argparse detox tox tox-pyenv
+
+echo "----> Pip Dependency Tree"
+pipdeptree
