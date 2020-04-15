1dce4e2ccedc0c9b79bd1529ace168d98c806c74
Andrew Gauld <agauld@att.com>
2020-04-15 19:00:40 +0000

    License-usage-manager cleanup
     - add code verify jobs for lum-server
     - add sonar job for lum-server
     - pass version into docker build
     - get docker tag from pom.xml
    
    Signed-off-by: Andrew Gauld <agauld@att.com>
    Issue-ID: ACUMOS-4114
    Change-Id: Iac54c1f6a1ccf3242ede7a9660d38486ab748e39

---
+++ jjb/license-usage-manager/tag-from-pom.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# For use as a JJB docker-get-container-tag-script
+# Use the project/version from a pom.xml as the tag for the docker image
+# By default, look for the pom.xml in the same directory as the Dockerfile.
+# This can be overridden by setting container-tag-yaml-dir
+
+set -feu -o pipefail
+
+echo "--> tag-from-pom.sh"
+
+tag=$(xmlstarlet sel -N "x=http://maven.apache.org/POM/4.0.0" -t -m "/x:project" -v x:version ${CONTAINER_TAG_YAML_DIR:-$DOCKER_ROOT}/pom.xml)
+
+echo "INFO: Docker tag is $tag"
+
+echo "DOCKER_IMAGE_TAG=$tag" >> "$WORKSPACE/env_docker_inject.txt"
