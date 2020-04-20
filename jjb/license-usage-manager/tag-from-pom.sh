5a3842c9f720df09faa0319f56a34c6371bb80be
Andrew Gauld <agauld@att.com>
2020-04-20 13:46:42 +0000

    Strip -SNAPSHOT from image tag
    Change-Id: Ic2618e293b0433f89958380c67c9ef7cfc686b49
    Issue-ID: ACUMOS-4114
    Signed-off-by: Andrew Gauld <agauld@att.com>

--- jjb/license-usage-manager/tag-from-pom.sh
+++ jjb/license-usage-manager/tag-from-pom.sh
@@ -11,6 +11,7 @@ set -feu -o pipefail
 echo "--> tag-from-pom.sh"
 
 tag=$(xmlstarlet sel -N "x=http://maven.apache.org/POM/4.0.0" -t -m "/x:project" -v x:version ${CONTAINER_TAG_YAML_DIR:-$DOCKER_ROOT}/pom.xml)
+tag=${tag%-*}
 
 echo "INFO: Docker tag is $tag"
 
