bc62e44c22ce2bc8705764a7f7cad421ad4c0d4f
Andrew Gauld <agauld@att.com>
2020-04-20 13:46:42 +0000

    Strip -SNAPSHOT from image tag
    Change-Id: Ic2618e293b0433f89958380c67c9ef7cfc686b49
    Issue-ID: ACUMOS-4114
    Signed-off-by: Andrew Gauld <agauld@att.com>

--- jjb/license-usage-manager/tag-from-pom.sh
+++ jjb/license-usage-manager/tag-from-pom.sh
@@ -5,12 +5,18 @@
 # Use the project/version from a pom.xml as the tag for the docker image
 # By default, look for the pom.xml in the same directory as the Dockerfile.
 # This can be overridden by setting container-tag-yaml-dir
+# This is used in a mixed-language project.  The Java component uses the
+# Maven convention of having -SNAPSHOT on version numbers with stage-release.
+# The non-Java components don't have stage-release so the -SNAPSHOT is
+# stripped from the pom.xml file's version number, when building those
+# components, here
 
 set -feu -o pipefail
 
 echo "--> tag-from-pom.sh"
 
 tag=$(xmlstarlet sel -N "x=http://maven.apache.org/POM/4.0.0" -t -m "/x:project" -v x:version ${CONTAINER_TAG_YAML_DIR:-$DOCKER_ROOT}/pom.xml)
+tag=${tag%-*}
 
 echo "INFO: Docker tag is $tag"
 
