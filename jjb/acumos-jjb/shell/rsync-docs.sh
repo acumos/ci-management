2d88d62dd16d11b182a61ca583a44b9a7552fefd
Anil Belur <abelur@linuxfoundation.org>
2020-09-03 18:19:06 +1000

    Auto-generated patch to resolve prettier/yamllint
    This is an auto-generated patch to handle prettier and yamllint rules.
    Please refer to the next patch that handles adding the prettier linter
    for the code behind this. Also update the .yamllint rules in
    preparation for prettier inclusion.
    
    Change-Id: I3c86022ec0ff84c515104ae2ed90783b89d7a758
    Signed-off-by: Anil Belur <abelur@linuxfoundation.org>

--- jjb/acumos-jjb/shell/rsync-docs.sh
+++ jjb/acumos-jjb/shell/rsync-docs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Ensure we fail the job if any steps fail
 set -e -x
-rsync -av --relative $DOC_DIR acumos-privdocs-rsync-elb-1197834036.us-west-2.elb.amazonaws.com::www/
+rsync -av --relative "$DOC_DIR" acumos-privdocs-rsync-elb-1197834036.us-west-2.elb.amazonaws.com::www/
